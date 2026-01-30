bf67d85bd96a01b62fd0951c091cf8c4be4c7bf5
tgaFFF <15302953668@163.com>
2026-01-31 05:04:11 +0800

    Delete api directory

--- api/text-proxy.js
+++
@@ -1,8 +0,0 @@
-import worker from '../cloudflare-worker.js';
-
-export default async function handler(request) {
-    const rewritten = new URL(request.url);
-    rewritten.pathname = '/text-proxy';
-    return worker.fetch(new Request(rewritten.toString(), request));
-}
-
